3ddf16aa7dc0e1a66d78ab2b50f130f48092bb29
intarikol <tarikol.islam@intellier.com>
2024-05-01 19:52:18 +0600

    Preparing the module for installation availableness.

---
+++ owl_components/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "owl_components",
+
+    'summary': "Basic Owl Components",
+
+    'description': """
+        Basic Owl Components, those are provide by built-in.
+    """,
+
+    'author': "Tarikol-Islam",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales/Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'owl_components/static/src/**/*',
+        ],
+    },
+}
+
